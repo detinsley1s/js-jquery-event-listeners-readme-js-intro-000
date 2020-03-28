123936fc1a8a465050885ca11246f134112c4a7b
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-28 05:20:43 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -7,6 +7,14 @@ function frameIt() {
   $('img').addClass('tasty')
 }
 
+function pressIt() {
+  $('form').on('keydown', key => {
+    if(key.which === 'G') {
+      alert('You pressed the G key')
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
